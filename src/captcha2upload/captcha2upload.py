01ad21bc52488308da3aa7a495812de7ba94fb1c
Thomanphan <Thomanphan@users.noreply.github.com>
2018-09-11 11:21:04 +0700

    Update captcha2upload.py

--- src/captcha2upload/captcha2upload.py
+++ src/captcha2upload/captcha2upload.py
@@ -5,8 +5,8 @@ from time import sleep
 
 class CaptchaUpload:
     def __init__(self, key, log=None, waittime=None):
-        self.settings = {"url_request": "http://2captcha.com/in.php",
-                         "url_response": "http://2captcha.com/res.php",
+        self.settings = {"url_request": "http://azcaptcha.com/in.php",
+                         "url_response": "http://azcaptcha.com/res.php",
                          "key": key}
         if log:
             self.log = log
@@ -29,16 +29,16 @@ class CaptchaUpload:
         request = get(fullurl)
         if "." in request.text:
             if self.logenabled:
-                self.log.info("[2CaptchaUpload] Balance: %s" % request.text)
+                self.log.info("[AZCaptchaUpload] Balance: %s" % request.text)
             return request.text
         elif request.text == "ERROR_KEY_DOES_NOT_EXIST":
             if self.logenabled:
-               self.log.error("[2CaptchaUpload] You used the wrong key in "
+               self.log.error("[AZCaptchaUpload] You used the wrong key in "
                              "the query")
             return 1
         elif request.text == "ERROR_WRONG_ID_FORMAT":
             if self.logenabled:
-                self.log.error("[2CaptchaUpload] Wrong format ID CAPTCHA. "
+                self.log.error("[AZCaptchaUpload] Wrong format ID CAPTCHA. "
                                "ID must contain only numbers")
             return 1
 
@@ -49,35 +49,35 @@ class CaptchaUpload:
         :return: <captchaword> OK | 1 ERROR!
         """
         if self.logenabled:
-            self.log.info("[2CaptchaUpload] Wait %s second.." % self.waittime)
+            self.log.info("[AZCaptchaUpload] Wait %s second.." % self.waittime)
         sleep(self.waittime)
         fullurl = "%s?key=%s&action=get&id=%s" % (self.settings['url_response'],
                                                   self.settings['key'], id)
         if self.logenabled:
-            self.log.info("[2CaptchaUpload] Get Captcha solved with id %s"
+            self.log.info("[AZCaptchaUpload] Get Captcha solved with id %s"
                           % id)
         request = get(fullurl)
         if request.text.split('|')[0] == "OK":
             return request.text.split('|')[1]
         elif request.text == "CAPCHA_NOT_READY":
             if self.logenabled:
-                self.log.error("[2CaptchaUpload] CAPTCHA is being solved, "
+                self.log.error("[AZCaptchaUpload] CAPTCHA is being solved, "
                               "repeat the request several seconds later, wait "
                               "another %s seconds" % self.waittime)
             return self.getresult(id)
         elif request.text == "ERROR_KEY_DOES_NOT_EXIST":
             if self.logenabled:
-               self.log.error("[2CaptchaUpload] You used the wrong key in "
+               self.log.error("[AZCaptchaUpload] You used the wrong key in "
                              "the query")
             return 1
         elif request.text == "ERROR_WRONG_ID_FORMAT":
             if self.logenabled:
-                self.log.error("[2CaptchaUpload] Wrong format ID CAPTCHA. "
+                self.log.error("[AZCaptchaUpload] Wrong format ID CAPTCHA. "
                                "ID must contain only numbers")
             return 1
         elif request.text == "ERROR_CAPTCHA_UNSOLVABLE":
             if self.logenabled:
-                self.log.error("[2CaptchaUpload] Captcha could not solve "
+                self.log.error("[AZCaptchaUpload] Captcha could not solve "
                                "three different employee. Funds for this "
                                "captcha not")
             return 1
@@ -93,66 +93,66 @@ class CaptchaUpload:
             files = {'file': open(pathfile, 'rb')}
             payload = {'key': self.settings['key'], 'method': 'post'}
             if self.logenabled:
-                self.log.info("[2CaptchaUpload] Uploading to 2Captcha.com..")
+                self.log.info("[AZCaptchaUpload] Uploading to 2Captcha.com..")
             request = post(self.settings['url_request'], files=files,
                            data=payload)
             if request.ok:
                 if request.text.split('|')[0] == "OK":
                     if self.logenabled:
-                        self.log.info("[2CaptchaUpload] Upload Ok")
+                        self.log.info("[AZCaptchaUpload] Upload Ok")
                     return self.getresult(request.text.split('|')[1])
                 elif request.text == "ERROR_WRONG_USER_KEY":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] Wrong 'key' parameter"
+                        self.log.error("[AZCaptchaUpload] Wrong 'key' parameter"
                                        " format, it should contain 32 symbols")
                     return 1
                 elif request.text == "ERROR_KEY_DOES_NOT_EXIST":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] The 'key' doesn't "
+                        self.log.error("[AZCaptchaUpload] The 'key' doesn't "
                                        "exist")
                     return 1
                 elif request.text == "ERROR_ZERO_BALANCE":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] You don't have money "
+                        self.log.error("[AZCaptchaUpload] You don't have money "
                                        "on your account")
                     return 1
                 elif request.text == "ERROR_NO_SLOT_AVAILABLE":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] The current bid is "
+                        self.log.error("[AZCaptchaUpload] The current bid is "
                                        "higher than the maximum bid set for "
                                        "your account.")
                     return 1
                 elif request.text == "ERROR_ZERO_CAPTCHA_FILESIZE":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] CAPTCHA size is less"
+                        self.log.error("[AZCaptchaUpload] CAPTCHA size is less"
                                        " than 100 bites")
                     return 1
                 elif request.text == "ERROR_TOO_BIG_CAPTCHA_FILESIZE":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] CAPTCHA size is more"
+                        self.log.error("[AZCaptchaUpload] CAPTCHA size is more"
                                        " than 100 Kbites")
                     return 1
                 elif request.text == "ERROR_WRONG_FILE_EXTENSION":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] The CAPTCHA has a "
+                        self.log.error("[AZCaptchaUpload] The CAPTCHA has a "
                                        "wrong extension. Possible extensions "
                                        "are: jpg,jpeg,gif,png")
                     return 1
                 elif request.text == "ERROR_IMAGE_TYPE_NOT_SUPPORTED":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] The server cannot "
+                        self.log.error("[AZCaptchaUpload] The server cannot "
                                        "recognize the CAPTCHA file type.")
                     return 1
                 elif request.text == "ERROR_IP_NOT_ALLOWED":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] The request has sent "
+                        self.log.error("[AZCaptchaUpload] The request has sent "
                                        "from the IP that is not on the list of"
                                        " your IPs. Check the list of your IPs "
                                        "in the system.")
                     return 1
                 elif request.text == "IP_BANNED":
                     if self.logenabled:
-                        self.log.error("[2CaptchaUpload] The IP address you're"
+                        self.log.error("[AZCaptchaUpload] The IP address you're"
                                        " trying to access our server with is "
                                        "banned due to many frequent attempts "
                                        "to access the server using wrong "
@@ -163,5 +163,5 @@ class CaptchaUpload:
 
         else:
             if self.logenabled:
-                self.log.error("[2CaptchaUpload] File %s not exists" % pathfile)
+                self.log.error("[AZCaptchaUpload] File %s not exists" % pathfile)
             return 1
