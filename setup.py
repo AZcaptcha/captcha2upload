da9385eff4e5bc06f77492780c428181676dfafd
Thomanphan <Thomanphan@users.noreply.github.com>
2018-09-11 11:22:25 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     package_dir={'captcha2upload': 'src/captcha2upload'},
     version='0.2',
     install_requires=['requests'],
-    description='Upload your image and solve captche using the 2Captcha '
+    description='Upload your image and solve captche using the AZCaptcha.com '
                   'Service',
-    author='Alessandro Sbarbati',
-    author_email='miriodev@gmail.com',
-    url='https://github.com/Mirio/captcha2upload',
-    download_url='https://github.com/Mirio/captcha2upload/tarball/0.1',
+    author='Thomanphan',
+    author_email='mail@azcaptcha.com',
+    url='https://github.com/Thomanphan/captcha2upload',
+    download_url='https://github.com/Thomanphan/captcha2upload/tarball/0.1',
     keywords=['2captcha', 'captcha', 'Image Recognition'],
     classifiers=["Topic :: Scientific/Engineering :: Image Recognition"],
 )
